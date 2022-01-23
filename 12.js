ef7c765993a14fc735117e39d6a472986c83e9c4
Zokirkhon <78657264+Zokirkhon1002@users.noreply.github.com>
2022-01-23 12:50:27 +0900

    added another example for greedy algorithms

--- 12.js
+++ 12.js
@@ -47,3 +47,24 @@
 // }
 // console.log(`Tashqi loop: ${x}`)
 // console.log(`Ichki loop: ${y}`)
+
+
+// another example
+function makeChange(amount, coins, bag = []) {
+  if (amount === 0) return bag
+  let largestCoin = getLargestCoin(amount, coins)
+  return makeChange(amount - largestCoin, coins, bag.concat([largestCoin]))
+}
+
+function getLargestCoin(amount, coins) {
+  let sortedCoins = coins.sort((a, b) => a - b)
+  for (let i = sortedCoins.length - 1; i >= 0; i--) {
+    if (sortedCoins[i] <= amount) return sortedCoins[i]
+  }
+  throw new Error('no coin that divides amount')
+}
+
+console.log(
+  makeChange(52, [1, 5, 10, 25])
+)
+// [25, 10, 5, 1, 1]
